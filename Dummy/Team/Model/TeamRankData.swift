2804e0b0448019e671099d5f47022aa1f124396f
sajid <sajid.goldmedal@gmail.com>
2022-04-17 17:12:29 +0530

    All changes

--- Dummy/Team/Model/TeamRankData.swift
+++ Dummy/Team/Model/TeamRankData.swift
@@ -6,32 +6,178 @@
 //
 
 import Foundation
-struct TeamRankData : Codable {
+struct TeamRank: Codable {
 
-        let fees : Double?
-        let id : Int?
-        let lName : String?
-        let state : Int?
-        let status : Int?
-        let winning : Double?
+    let id: Int?
+    let LName: String?
+    let Winning: Double?
+    let ltype: Int?
+    let fees: Double?
+    let state: Int?
+    let Status: Int?
+    let LMaxSize: Int?
+    let LCurSize: Int?
+    let wData: LeagueWData?
+    let awards: [LeagueAwards]?
 
-        enum CodingKeys: String, CodingKey {
-                case fees = "fees"
-                case id = "id"
-                case lName = "LName"
-                case state = "state"
-                case status = "Status"
-                case winning = "Winning"
-        }
-    
-        init(from decoder: Decoder) throws {
-                let values = try decoder.container(keyedBy: CodingKeys.self)
-                fees = try values.decodeIfPresent(Double.self, forKey: .fees)
-                id = try values.decodeIfPresent(Int.self, forKey: .id)
-                lName = try values.decodeIfPresent(String.self, forKey: .lName)
-                state = try values.decodeIfPresent(Int.self, forKey: .state)
-                status = try values.decodeIfPresent(Int.self, forKey: .status)
-                winning = try values.decodeIfPresent(Double.self, forKey: .winning)
-        }
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case LName = "LName"
+        case Winning = "Winning"
+        case ltype = "ltype"
+        case fees = "fees"
+        case state = "state"
+        case Status = "Status"
+        case LMaxSize = "LMaxSize"
+        case LCurSize = "LCurSize"
+        case wData = "wData"
+        case awards = "awards"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(Int.self, forKey: .id)
+        LName = try values.decodeIfPresent(String.self, forKey: .LName)
+        Winning = try values.decodeIfPresent(Double.self, forKey: .Winning)
+        ltype = try values.decodeIfPresent(Int.self, forKey: .ltype)
+        fees = try values.decodeIfPresent(Double.self, forKey: .fees)
+        state = try values.decodeIfPresent(Int.self, forKey: .state)
+        Status = try values.decodeIfPresent(Int.self, forKey: .Status)
+        LMaxSize = try values.decodeIfPresent(Int.self, forKey: .LMaxSize)
+        LCurSize = try values.decodeIfPresent(Int.self, forKey: .LCurSize)
+        wData = try values.decodeIfPresent(LeagueWData.self, forKey: .wData)
+        awards = try values.decodeIfPresent([LeagueAwards].self, forKey: .awards)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(LName, forKey: .LName)
+        try container.encode(Winning, forKey: .Winning)
+        try container.encode(ltype, forKey: .ltype)
+        try container.encode(fees, forKey: .fees)
+        try container.encode(state, forKey: .state)
+        try container.encode(Status, forKey: .Status)
+        try container.encode(LMaxSize, forKey: .LMaxSize)
+        try container.encode(LCurSize, forKey: .LCurSize)
+        try container.encode(wData, forKey: .wData)
+        try container.encode(awards, forKey: .awards)
+    }
 
 }
+
+/*
+struct TeamRankWData: Codable {
+
+    let ent: Int?
+    let prz: Int?
+    let w: Int?
+    let rnk: [TeamRnk]?
+
+    private enum CodingKeys: String, CodingKey {
+        case ent = "ent"
+        case prz = "prz"
+        case w = "w"
+        case rnk = "rnk"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        ent = try values.decodeIfPresent(Int.self, forKey: .ent)
+        prz = try values.decodeIfPresent(Int.self, forKey: .prz)
+        w = try values.decodeIfPresent(Int.self, forKey: .w)
+        rnk = try values.decodeIfPresent([TeamRnk].self, forKey: .rnk)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(ent, forKey: .ent)
+        try container.encode(prz, forKey: .prz)
+        try container.encode(w, forKey: .w)
+        try container.encode(rnk, forKey: .rnk)
+    }
+
+}
+
+struct TeamRnk: Codable {
+
+    let o: String?
+    let coin: Int?
+    let prz: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case o = "o"
+        case coin = "coin"
+        case prz = "prz"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        o = try values.decodeIfPresent(String.self, forKey: .o)
+        coin = try values.decodeIfPresent(Int.self, forKey: .coin)
+        prz = try values.decodeIfPresent(String.self, forKey: .prz)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(o, forKey: .o)
+        try container.encode(coin, forKey: .coin)
+        try container.encode(prz, forKey: .prz)
+    }
+
+}
+
+
+struct TeamAwards: Codable {
+
+    let rnk: Int?
+    let a: [TeamAwardDetail]?
+
+    private enum CodingKeys: String, CodingKey {
+        case rnk = "rnk"
+        case a = "a"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        rnk = try values.decodeIfPresent(Int.self, forKey: .rnk)
+        a = try values.decodeIfPresent([TeamAwardDetail].self, forKey: .a)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(rnk, forKey: .rnk)
+        try container.encode(a, forKey: .a)
+    }
+
+}
+
+
+struct TeamAwardDetail: Codable {
+
+    let itm: String?
+    let desc: String?
+    let img: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case itm = "itm"
+        case desc = "desc"
+        case img = "img"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        itm = try values.decodeIfPresent(String.self, forKey: .itm)
+        desc = try values.decodeIfPresent(String.self, forKey: .desc)
+        img = try values.decodeIfPresent(String.self, forKey: .img)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(itm, forKey: .itm)
+        try container.encode(desc, forKey: .desc)
+        try container.encode(img, forKey: .img)
+    }
+
+}
+*/
